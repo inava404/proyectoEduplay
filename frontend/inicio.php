244bfd98b20e39b4a9411acc8fdd1c6304b248b9
inava <144198990+inava404@users.noreply.github.com>
2024-11-25 13:06:48 -0600

    Creacion de archivo php principal y estilo

---
+++ frontend/inicio.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eduplay</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos/estilo.css"> 
+</head>
+<body>
+    
+</body>
+</html>
