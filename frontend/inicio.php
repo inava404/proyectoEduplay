c6e60b9378e35d7162c26ed6f84e096e244c6a92
inava <144198990+inava404@users.noreply.github.com>
2024-11-26 22:43:58 -0600

    Primer avance

--- frontend/inicio.php
+++ frontend/inicio.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eduplay</title>
-    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
-    <link rel="stylesheet" href="estilos/estilo.css"> 
+    <link rel="stylesheet" href="src/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="src/estilo.css">
 </head>
 <body>
-    
-</body>
-</html>
+    <nav class="navbar">
+      <img src="src/img/EduplayLogo.png" alt="Eduplay Logo" width="375px">
+    </nav>
+
+    <div class="section-inicio"> 
+        <h1>¡BIENVENIDO!</h1>
+    </div>
+    <div class="section-inicio-btns">
+        <button style="margin-right:30px" type="button" class="btn-inicio">Inicio de Sesión</button>
+        <button id="request-loan" class="btn btn-success"> Registro </button>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src=""></script>
+  </body>
+  </html>
