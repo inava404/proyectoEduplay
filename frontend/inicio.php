894400d8d54ba0c9397a30e21f5e92ddaa4ddafe
inava <144198990+inava404@users.noreply.github.com>
2024-11-28 19:31:32 -0600

    Pantalla Principal de Incio hecha

--- frontend/inicio.php
+++ frontend/inicio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eduplay</title>
-    <link rel="stylesheet" href="src/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
     <link rel="stylesheet" href="src/estilo.css">
 </head>
 <body>
@@ -16,8 +16,8 @@
         <h1>¡BIENVENIDO!</h1>
     </div>
     <div class="section-inicio-btns">
-        <button style="margin-right:30px" type="button" class="btn-inicio">Inicio de Sesión</button>
-        <button id="request-loan" class="btn btn-success"> Registro </button>
+      <a href="#"><span>Iniciar Sesión</span></a>
+      <a href="#"><span>Registrarse</span></a>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"
