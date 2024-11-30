b372a592935f8ae53da0902265905a889a612709
inava <144198990+inava404@users.noreply.github.com>
2024-11-29 19:02:44 -0600

    Login y Signup añadidos (Unicamente diseño)

---
+++ frontend/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eduplay</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
+    <link rel="stylesheet" href="src/estilo.css">
+</head>
+<body>
+    <nav class="navbar">
+      <img src="src/img/EduplayLogo.png" alt="Eduplay Logo" width="375px">
+    </nav>
+
+    <div class="wrapper"> 
+      <h1>Iniciar Sesión</h1>
+      <form action="#">
+        <input type="text" placeholder="Email">
+        <input type="password" placeholder="Contraseña">
+        <div class="recover">
+          <a href="#">¿Olvidaste tu contraseña?</a>
+        </div>
+      </form>
+      <div class="section-inicio-btns">
+        <a href="#"><span>Iniciar Sesión</span></a>
+      </div>
+      <div class="member">
+        ¿No tienes cuenta? <a href="signup.php">
+          ¡Registrate aquí!
+        </a>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src=""></script>
+  </body>
+  </html>
---
+++ frontend/signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eduplay</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
+    <link rel="stylesheet" href="src/estilo.css">
+</head>
+<body>
+    <nav class="navbar">
+      <img src="src/img/EduplayLogo.png" alt="Eduplay Logo" width="375px">
+    </nav>
+
+    <div class="wrapper"> 
+      <h1>Registrarse</h1>
+      <form action="#">
+        <input type="text" placeholder="Email">
+        <input type="text" placeholder="Nombre(s)">
+        <input type="text" placeholder="Apellido">
+        <input type="text" placeholder="Fecha de Nacimiento">
+        <input type="password" placeholder="Contraseña">
+        <input type="password" placeholder="Confirmar contraseña">
+      </form>
+      <div class="section-inicio-btns">
+        <a href="#"><span>Registrarme</span></a>
+      </div>
+      <div class="member">
+        ¿Ya tienes cuenta? <a href="login.php">
+          ¡Inicia sesión aquí!
+        </a>
+      </div>        
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src=""></script>
+  </body>
+  </html>
