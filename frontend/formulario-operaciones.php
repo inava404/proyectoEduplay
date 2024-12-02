3e6de9a16c1860be0810a9c4a7cfee76a110b6b8
inava <144198990+inava404@users.noreply.github.com>
2024-12-01 21:02:50 -0600

    Matematicas - Form de operaciones basicas terminado

--- frontend/formulario-operaciones.php
+++ frontend/formulario-operaciones.php
@@ -29,63 +29,64 @@
 
     <div class="formulario-materias">
         <div class="materias-wrapper">
-            <form>
-                <div class="form-group">
-                    <label>¿Cómo se llama el lugar donde estudias?</label><br>
-                    <div class="form-images">
-                        <img src="src/img/parque.png" alt="Parque">
-                        <img src="src/img/escuela.png" alt="Escuela">
-                        <img src="src/img/casa.png" alt="Casa">
-                        <img src="src/img/tienda.png" alt="Tienda">
-                    </div>
-                    <input type="radio" name="pregunta1" value="A"> A) Parque<br>
-                    <input type="radio" name="pregunta1" value="B"> B) Escuela<br>
-                    <input type="radio" name="pregunta1" value="C"> C) Casa<br>
-                    <input type="radio" name="pregunta1" value="D"> D) Tienda
+        <form id="math-addition-subtraction">
+            <div class="form-group">
+                <label>¿Cuánto es 2 + 3?</label>
+                <div class="form-images">
+                    <img src="src/img/2.png" alt="Dos">
+                    <img src="src/img/mas.png" alt="Suma">
+                    <img src="src/img/3.png" alt="Tres">
                 </div>
+                <input type="radio" name="pregunta1" value="A"> A) 4<br>
+                <input type="radio" name="pregunta1" value="B"> B) 5<br>
+                <input type="radio" name="pregunta1" value="C"> C) 6<br>
+                <input type="radio" name="pregunta1" value="D"> D) 7
+            </div>
 
-                <div class="form-group">
-                    <label>¿Qué palabra es el nombre de un juguete?</label>
-                    <div class="form-images">
-                        <img src="src/img/pelota.png" alt="Pelota">
-                    </div>
-                    <input type="radio" name="pregunta2" value="A"> A) Pelota<br>
-                    <input type="radio" name="pregunta2" value="B"> B) Escuela<br>
-                    <input type="radio" name="pregunta2" value="C"> C) Avión<br>
-                    <input type="radio" name="pregunta2" value="D"> D) Sol
+            <div class="form-group">
+                <label>Si tienes 5 manzanas y comes 2, ¿cuántas te quedan?</label>
+                <div class="form-images">
+                    <img src="src/img/manzanaRoja.png" alt="Manzana">
+                    <img src="src/img/manzanaRoja.png" alt="Manzana">
+                    <img src="src/img/manzanaRoja.png" alt="Manzana">
+                    <img src="src/img/manzanaRoja.png" alt="Manzana">
+                    <img src="src/img/manzanaRoja.png" alt="Manzana">
                 </div>
+                <input type="radio" name="pregunta2" value="A"> A) 2<br>
+                <input type="radio" name="pregunta2" value="B"> B) 3<br>
+                <input type="radio" name="pregunta2" value="C"> C) 4<br>
+                <input type="radio" name="pregunta2" value="D"> D) 5
+            </div>
 
-                <div class="form-group">
-                    <label>¿Qué haces cuando tienes sueño?</label>
-                    <div class="form-images">
-                        <img src="src/img/correr.png" alt="Correr">
-                        <img src="src/img/nadar.png" alt="Nadar">
-                        <img src="src/img/dormir.png" alt="Dormir">
-                        <img src="src/img/jugar.png" alt="Jugar">
-                    </div>
-                    <input type="radio" name="pregunta3" value="A"> A) Correr<br>
-                    <input type="radio" name="pregunta3" value="B"> B) Nadar<br>
-                    <input type="radio" name="pregunta3" value="C"> C) Dormir<br>
-                    <input type="radio" name="pregunta3" value="D"> D) Jugar
+            <div class="form-group">
+                <label>Si tienes 2 perros y llega otro más, ¿Cuántos perros tienes ahora?</label>
+                <div class="form-images">
+                    <img src="src/img/perro.png" alt="Perro uno">
+                    <img src="src/img/perro1.png" alt="Perro dos">
+                    <img src="src/img/perro2.png" alt="Perro tres">
                 </div>
+                <input type="radio" name="pregunta3" value="A"> A) 4<br>
+                <input type="radio" name="pregunta3" value="B"> B) 3<br>
+                <input type="radio" name="pregunta3" value="C"> C) 5<br>
+                <input type="radio" name="pregunta3" value="D"> D) 2
+            </div>
 
-                <div class="form-group">
-                    <label>¿Con que escribes?</label>
-                    <div class="form-images">
-                        <img src="src/img/lapiz.png" alt="Lapiz">
-                        <img src="src/img/goma.png" alt="Goma">
-                        <img src="src/img/tijeras.png" alt="Tijeras">
-                        <img src="src/img/pegamento.png" alt="Pegamento">
-                    </div>
-                    <input type="radio" name="pregunta4" value="A"> A) Lapiz<br>
-                    <input type="radio" name="pregunta4" value="B"> B) Goma<br>
-                    <input type="radio" name="pregunta4" value="C"> C) Pegamento<br>
-                    <input type="radio" name="pregunta4" value="D"> D) Tijeras
+            <div class="form-group">
+                <label>¿Cuánto es 6 - 4?</label>
+                <div class="form-images">
+                    <img src="src/img/6.png" alt="Seis">
+                    <img src="src/img/menos.png" alt="Resta">
+                    <img src="src/img/4.png" alt="Cuatro">
                 </div>
-                <div class="form-btns">
-                    <a href="#"><span>¡He terminado!</span></a>
-                </div>
-            </form>
+                <input type="radio" name="pregunta4" value="A"> A) 1<br>
+                <input type="radio" name="pregunta4" value="B"> B) 2<br>
+                <input type="radio" name="pregunta4" value="C"> C) 3<br>
+                <input type="radio" name="pregunta4" value="D"> D) 4
+            </div>
+            <div class="form-btns">
+                <a href="#"><span>¡He terminado!</span></a>
+            </div>
+        </form>
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"
